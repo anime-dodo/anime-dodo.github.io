e241e6fdd8aabfcd090381cce00c6ce4a456cf71
lfyzyx <Groscranepoiludu91@gmail.com>
2026-08-29 08:02:23 +0200

    Corrige double-clic plein ecran + ajoute bouton play manuel

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3621054136" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1103775216" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
