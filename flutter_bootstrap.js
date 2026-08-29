bf960e6451a99bad8272885b01b29762623ac41e
lfyzyx <Groscranepoiludu91@gmail.com>
2026-08-29 05:05:11 +0200

    Continuer a regarder : un seul par anime, croix, badge VF/VOSTFR

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "606553232" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1307492476" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
